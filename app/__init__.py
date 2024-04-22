6aa01ba2a7c9dabdf62019d67515fc98cce82f1a
sersangn <sersang01@gmail.com>
2024-04-22 15:16:00 +0800

    Added modules to the requirements.txt file and added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
-from app import routes
+from app import routes, models
+
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
