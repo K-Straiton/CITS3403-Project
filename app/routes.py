ef19db63ca55d10d87a199b0562465dacccc93d2
Seb <bones@ucc.asn.au>
2024-04-24 22:10:44 +0800

    Add profile page to forms
    Sign-in now redirects to profile page with username and pronouns

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, url_for, request
 from app import app
 from app.forms import *
 import pprint
@@ -22,7 +22,19 @@ def loginPage():
 def signUpPage():
     form = SignUpForm()
     if form.validate_on_submit():
-        flash('Sign up requested for user {}, pronouns={}, you def can\'t read my password. Wait. Shi- {}'.format(form.username.data, form.pronouns.data, form.password.data))
-        return redirect('/index')
+        # Username = form.username.data
+        # Pronouns = form.pronouns.data
+        # session['Username'] = form.username.data
+        # session['Pronouns'] = form.pronouns.data
+        # return redirect(url_for('.profileFunc', Username=Username, Pronouns=Pronouns))
+        return redirect(url_for('profilePage', name=form.username.data, pronouns=form.pronouns.data))
 
     return render_template("sign-up.html", title="Sign Up", form=form)
+
+
+@app.route('/profile')
+def profilePage():
+    name = request.args.get('name', None)
+    pronouns = request.args.get('pronouns')
+
+    return render_template("profile.html", name=name, pronouns=pronouns)
