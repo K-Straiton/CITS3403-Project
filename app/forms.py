ee19eda5b69fa32e2ec57a933884555b539c0a2d
Seb <bones@ucc.asn.au>
2024-04-24 18:26:45 +0800

    Setup a signup form and setup test for sanity for post requests
    Sign-up redirects on valid submission to inddex.html with a printout of values - still need to hash password

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired, InputRequired
 
 class SignInForm(FlaskForm):
 	username = StringField("Username",validators=[DataRequired()])
@@ -11,4 +11,5 @@ class SignUpForm(FlaskForm):
 	username = StringField("Username",validators=[DataRequired()])
 	password = StringField("Password",validators=[DataRequired()])
 	signUp = SubmitField("Sign Up")
+	pronouns = RadioField("Pronouns:", validators=[InputRequired(message=None)], choices=[("She/Her", "She/Her"), ("He/Him", "He/Him"), ("They/Them","They/Them")])
 #   STILL NEED TO ADD PRONOUNS TO THIS FORM
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import *
+import pprint
+
 
 
 @app.route('/')
@@ -13,7 +15,11 @@ def loginPage():
 	form = SignInForm()
 	return render_template("login.html", title='Log In', form=form)
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def signUpPage():
     form = SignUpForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, pronouns={}, you def can\'t read my password. Wait. Shi- {}'.format(form.username.data, form.pronouns.data, form.password.data))
+        return redirect('/index')
+
     return render_template("sign-up.html", title="Sign Up", form=form)
