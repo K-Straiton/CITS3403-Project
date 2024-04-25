81414bc06469acd118cb98e305d02f958a907b06
sersang <sersang01@gmail.com>
2024-04-25 16:55:18 +0800

    Added pronouns to database

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    
+    pronouns: so.Mapped[str] = so.mapped_column(sa.String(20))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
