12617c98cf046c903a63b8cd6667cd1dbc2436de
sersang <sersang01@gmail.com>
2024-04-25 17:02:03 +0800

    Cleaned up init file

--- app/__init__.py
+++ app/__init__.py
@@ -11,21 +11,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+from app import routes, models, forms
 
-
-# from flask import Flask
-# app = Flask(__name__)
-# from app import routes, models
-
-# from app.config import Config
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-from app import forms
 
