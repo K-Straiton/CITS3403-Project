3deaf16d2ebffc63151a9d7def13c9251e67c3b8
Seb <bones@ucc.asn.au>
2024-05-12 19:12:10 +0800

    Fix home redirect issue when not logged in

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request
+from flask import render_template, redirect, session, url_for, request
 from app import app
 from app.forms import *
 #from werkzeug.security import generate_password_hash, check_password_hash
@@ -32,7 +32,6 @@ def loginPage():
     if form.validate_on_submit():
         user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
             return redirect(url_for('loginPage'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('profilePage'))
@@ -40,29 +39,21 @@ def loginPage():
 #        # hashedPassword = generate_password_hash('form.username.data')
 #        # password = generate_password_hash('test')
 #        # if(check_password_hash(password, form.password.data)):
-#        flash('Login requested for user {}, you def can\'t read my password. Wait. Shi- {}'.format(form.username.data, form.password.data))
 #        return redirect('/index')
     return render_template("login.html", title='Log In', form=form)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    if current_user.is_authenticated:
-        # name = request.args.get('name', None)
-        name = current_user.username
-        pronouns = current_user.pronouns
-        thinkpads = current_user.ThinkPads
-        postsNum = db.session.scalars(sa.select((func.count())).select_from(Post).where(Post.user_id==current_user.id)).all()[0]
-        form=newPost()
-        if form.validate_on_submit():
+    form=newPost()
+    if form.validate_on_submit():
+        if current_user.is_authenticated:
             post = Post(title=form.title.data, body=form.post.data, author=current_user)
             db.session.add(post)
             db.session.commit()
-            flash('Your post is now live!')
             return redirect(url_for('index'))
-    else:
-        return redirect(url_for('loginPage'))
-
+        else:
+            return redirect(url_for('loginPage'))
     posts = db.session.scalars(sa.select(Post).order_by(Post.timestamp.desc())).all()
     length = len(posts)+1
     commentsList = [0]*length
@@ -81,7 +72,6 @@ def signUpPage():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
         return redirect(url_for('loginPage'))
 
     return render_template("sign-up.html", title="Sign Up", form=form)
@@ -101,7 +91,6 @@ def profilePage():
             post = Post(title=form.title.data, body=form.post.data, author=current_user)
             db.session.add(post)
             db.session.commit()
-            flash('Your post is now live!')
             return redirect(url_for('profilePage'))
     else:
         return redirect(url_for('loginPage'))
