22ae09846788f39af2bb936884ede84185f22bfd
K-Straiton <straiton98@gmail.com>
2024-05-14 18:35:29 +0800

    Add tags table to models.py
    Co-authored-by: Seb <Sebagabones@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -38,6 +38,7 @@ class Post(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     author: so.Mapped[User] = so.relationship(back_populates='posts')
+    tags: so.Mapped[Tags] = so.relationship(back_populates='postTags')
     def __repr__(self):
         return '<Post {}>'.format(self.title, self.body)
     
@@ -49,10 +50,15 @@ class Comments(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     commentPoster: so.Mapped[User] = so.relationship(back_populates='userComments')
-
     def __repr__(self):
         return '<Comments {}>'.format(self.body)
 
+class Tags(db.Model):
+    reason: so.Mapped[str] = so.mapped_coloumn(sa.String(50))
+    model: so.Mapped[str] = so.mapped_coloumn(sa.String(50))
+    os: so.Mapped[str] = so.mapped_coloumn(sa.String(50))
+    postTags: so.Mapped[Post] = so.relationship(back_populates='tags')
+    
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
--- flaskApp.py
+++ flaskApp.py
@@ -2,8 +2,8 @@ from app import app
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
-from app.models import User, Post, Comments
+from app.models import User, Post, Comments, Tags
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post, 'Comments':Comments}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post, 'Comments':Comments, 'Tags':Tags}
