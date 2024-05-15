ea0fb7a0b61fc2622cd8266d4cb0eed6d41859e2
sersang <sersang01@gmail.com>
2024-05-15 19:34:56 +0800

    Added testing for adding user to db
    Co-authored-by: Seb <Sebagabones@users.noreply.github.com>

--- testing.py
+++ testing.py
@@ -4,10 +4,13 @@ os.environ['DATABASE_URL'] = 'sqlite://'
 from datetime import datetime, timezone, timedelta
 import unittest
 from app import app, db
+import sqlalchemy as sa
+
 from app.models import *
 
 
 class UserModelCase(unittest.TestCase):
+    
     def setUp(self):
         self.app_context = app.app_context()
         self.app_context.push()
@@ -24,5 +27,19 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u.check_password('dog'))
         self.assertTrue(u.check_password('cat'))
 
+    def test_addUserToDB(self):
+        user = User(username='sersangy', email='sersang@isawesome.com', pronouns="she/her", ThinkPads=0)
+        db.session.add(user)
+        db.session.commit()
+        users =  db.session.scalars(sa.select(User).select_from(User)).all()
+        self.assertIn(user, users)
+        
+
+#     def createPost():
+#         post = Post(title=form.title        post = Post(title=form.title.data, body=form.post.data, author=u.posts)
+# .data, body=form.post.data, author=u.posts)
+
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=5)
+    
+    
--- testing/unit.py
+++
@@ -1,4 +0,0 @@
-import unittest
-from app.models import *
-
-postsTestData = Post()
