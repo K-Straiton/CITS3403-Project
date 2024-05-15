78cffb7fe433f916c6caf5e8fa5c3e8ab124c16a
sersang <sersang01@gmail.com>
2024-05-15 20:02:10 +0800

    Added test for create posts
    Co-authored-by: Seb <Sebagabones@users.noreply.github.com>

--- testing.py
+++ testing.py
@@ -34,10 +34,20 @@ class UserModelCase(unittest.TestCase):
         users =  db.session.scalars(sa.select(User).select_from(User)).all()
         self.assertIn(user, users)
         
+    def test_createPost(self):
+        # u =  db.session.scalars(sa.select(User).select_from(User)).first()
+        u = User(username='sersangy', email='sersang@isawesome.com', pronouns="she/her", ThinkPads=0)
+        db.session.add(u)
+        db.session.commit()
+        post = Post(id = 1, title="test title", body="test body", author=u, user_id=u.id)
+        db.session.add(post)
+        db.session.commit()
+        posts = db.session.scalars(sa.select(Post).select_from(Post)).all()
+        self.assertIn(post, posts)
+    
+    # def test_addComment(self):
+    #     u = db.session.scalar(sa.select(Comments).select_from(User)).first()
 
-#     def createPost():
-#         post = Post(title=form.title        post = Post(title=form.title.data, body=form.post.data, author=u.posts)
-# .data, body=form.post.data, author=u.posts)
 
 if __name__ == '__main__':
     unittest.main(verbosity=5)
