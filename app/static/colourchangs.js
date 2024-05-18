3a4465585a28d695068eb1ec85a0f99bc8c8f80c
lauren <lhpudney@gmail.com>
2024-05-18 15:55:15 +0800

    randomise colour selection for username

--- app/static/colourchangs.js
+++ app/static/colourchangs.js
@@ -1,22 +1,28 @@
+const classArray = [
+  "link-red",
+  "link-orange",
+  "link-yellow",
+  "link-yellow",
+  "link-green",
+  "link-blue",
+];
+
 window.onload = function () {
   alert("Welcome to the page!");
 };
 function appendCardTitlesToList() {
   const Comments = document.querySelectorAll(".comment");
-  // Create an unordered list element
 
-  //   for (const comment of Comments) {
-  //     alert(comment.textContent);
-  //     //comment.style.colour = "red";
-  //   }
   Comments.forEach((Comments) => {
-    Comments.style.color = "red";
     const Links = Comments.querySelectorAll("a");
     Links.forEach((Links) => {
       alert(Links.textContent);
-      Links.classList.toggle("link-blue");
+      const randomIndex = Math.floor(Math.random() * classArray.length);
+      const colourClass = classArray[randomIndex];
+
+      Links.classList.toggle(colourClass);
     });
   });
 }
-// Call the function to execute the logic
+
 appendCardTitlesToList();
