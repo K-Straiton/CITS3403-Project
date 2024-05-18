d35f6250434121b1e604355a1450168057c1086f
sersang <sersang01@gmail.com>
2024-05-18 13:15:31 +0800

    added selenium initialization

--- selenium.py
+++ selenium.py
@@ -1,22 +1,10 @@
 #From the lecture
-
 from lib2to3.pgen2 import driver
 from operator import index
 from selenium.webdriver.support.ui import Select
 
-select = Select(driver.find_element_by_name('name'))
-select.select_by_index(index)
-select.select_by_visible_text("text")
-select.select_by_value(value)
-
-element = driver.find_element_by_name("source")
-target = driver.find_element_by_name("target")
-
-from selenium.webdriver import ActionChains
-action_chains = ActionChains(driver)
-action_chains.drag_and_drop(element,target).perform()
-
-
+ 
+#Unittest imports
 import os
 os.environ['DATABASE_URL'] = 'sqlite://'
 
@@ -24,11 +12,9 @@ from datetime import datetime, timezone, timedelta
 import unittest
 from app import app, db
 import sqlalchemy as sa
-
 from app.models import *
 
 localHost = "http://localhost:5000/"
-
 
 
 class SeleniumTests(TestCase):
