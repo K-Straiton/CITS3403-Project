3d8ea695eab8ac966739b1809f7974a3f5c2f5ab
lauren <lhpudney@gmail.com>
2024-05-19 11:31:23 +0800

    remove majority of errors from homepage

--- flaskApp.py
+++ flaskApp.py
@@ -3,6 +3,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post, Comments
+from flask_wtf.csrf import CSRFProtect
 
 @app.shell_context_processor
 def make_shell_context():
