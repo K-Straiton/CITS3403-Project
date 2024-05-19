27add7a03788759ccbe1819cafabbe0e9e40b0fb
K-Straiton <straiton98@gmail.com>
2024-05-19 15:13:43 +0800

    Fix multiple append issue

--- app/static/signup-password-validation.js
+++ app/static/signup-password-validation.js
@@ -16,14 +16,24 @@ form.addEventListener("submit", function (event) {
     }
     else {
 
-        password.classList.add('is-invalid');
-        confirmPassword.classList.add('is-invalid');
-        let errorField = document.createElement('div');
-        errorField.classList.add('invalid-feedback');
-        errorField.classList.add('mb-1');
-        errorField.textContent = "Passwords must match.";
-        let parentNode = confirmPassword.parentNode.parentNode;
-        parentNode.insertBefore(errorField, confirmPassword.parentNode.nextSibling);
-        errorField.style.display="block";
+        let errorField = document.getElementById("confirmPasswordError");
+
+        if(errorField == null) {
+            password.classList.add('is-invalid');
+            confirmPassword.classList.add('is-invalid');
+            errorField = document.createElement('div');
+            errorField.classList.add('invalid-feedback');
+            errorField.classList.add('mb-1');
+            errorField.id = "confirmPasswordError";
+            errorField.textContent = "Passwords must match.";
+            let parentNode = confirmPassword.parentNode.parentNode;
+            parentNode.insertBefore(errorField, confirmPassword.parentNode.nextSibling);
+            errorField.style.display="block";
+
+        }
+
+        password.value = '';
+        confirmPassword.value = '';
+
     }
 })
